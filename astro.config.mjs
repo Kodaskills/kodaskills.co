8962561ee0648f185470f32ed96c0f12353c5196
William Gentil <william.gentil@protonmail.com>
2025-05-14 14:18:27 +0200

    feat: ✨ ui components #36 (#44)
    * feat: ✨ add MDX
    
    * chore: 🚨 fix docker version warning
    
    * chore: 🚸 update contact section UX #36
    
    * chore: 🚸 update service section UX #36
    
    * feat: ✨ add hero section #36
    
    * refactor: 💬 add copyright and changed date experience #36
    
    * feat: ✨ add mega menu to header #36
    
    * refactor: 🎨 seperate Layout code for better readability and usability
    
    * feat: 🚸 improve user experience on contact page #36
    
    * feat: ✨ blog with mdx-posts rendering #36
    
    * fix: 🐛  fix date-bug on blog articles with US date format
    
    * fix: 🩹 missing comma for DaisyUI themes
    
    * refactor: 🎨 refactor code for contact button in header
    
    * feat: ✨ add service pages based on mdx files #36
    
    * fix: 🚧 knip unused files to fix #41
    
    * feat: ✨ add navigation for services and blog #36
    
    * refactor: 🔥  remove useless code
    
    * feat: ✨ add navigation for services #36
    
    * refactor: 🚚 rename article files order
    
    * feat: ✨ add nav for buttons and improve wireflow matching #36
    
    * refactor: 🚸 improve wireflow matching, 3rd card and nav for buttons #36
    
    * refactor: 💄add socials icons and navigation #36
    
    * refactor: 🔥 remove unnecessary code
    
    * feat: ✨ add about page #36
    
    * refactor: 💄 changed style to light for icon visualisation
    
    * refactor: 🔥 remove unnecessary comment #36
    
    * refactor: 🚚 rename files in lowercase #36
    
    ---------
    
    Co-authored-by: gentil <skabz@DESKTOP-8EG3JAF>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   server: { port: 4321, host: true},
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [mdx()]
 });
