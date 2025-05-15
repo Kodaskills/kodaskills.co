f6e63f829c9e5e06a173c2b3b727b0e2512d5b56
William Gentil <william.gentil@protonmail.com>
2025-05-15 10:11:27 +0200

    feat: ✨ add environment file #48 (#49)
    * refactor: 🔧 add .env.development file #48
    
    * chore: 🔧 ignore vite dependency for knip #48
    
    ---------
    
    Co-authored-by: Yudao <mickael.gentil@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import { loadEnv } from "vite";
 
-import tailwindcss from '@tailwindcss/vite';
-
-import mdx from '@astrojs/mdx';
+const { ASTRO_SERVER_PORT, ASTRO_SERVER_HOST } = loadEnv(
+  process.env.NODE_ENV || "development",
+  process.cwd(),
+  ""
+);
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 4321, host: true},
-
-  vite: {
-    plugins: [tailwindcss()]
+  server: {
+    port: Number.parseInt(ASTRO_SERVER_PORT),
+    host: Boolean(ASTRO_SERVER_HOST),
   },
 
-  integrations: [mdx()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [mdx()],
 });
