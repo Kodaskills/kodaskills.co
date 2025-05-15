048dd45c9d2d651c9ad71277daec712aeff62dff
Yudao <mickael.gentil@gmail.com>
2025-05-15 16:51:19 +0200

    chore: 🔧 update environment variables #50

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,18 @@ import tailwindcss from "@tailwindcss/vite";
 import mdx from "@astrojs/mdx";
 import { loadEnv } from "vite";
 
-const { ASTRO_SERVER_PORT, ASTRO_SERVER_HOST } = loadEnv(
-  process.env.NODE_ENV || "development",
-  process.cwd(),
-  ""
-);
+const { ASTRO_SERVER_PORT, ASTRO_SERVER_HOST, ASTRO_SERVER_PREVIEW_PORT } =
+  loadEnv(process.env.NODE_ENV || "development", process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: Number.parseInt(ASTRO_SERVER_PORT),
+  server: ({ command }) => ({
+    port:
+      command === "preview"
+        ? Number.parseInt(ASTRO_SERVER_PREVIEW_PORT)
+        : Number.parseInt(ASTRO_SERVER_PORT),
     host: Boolean(ASTRO_SERVER_HOST),
-  },
-
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
