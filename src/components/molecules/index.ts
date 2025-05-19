2ecba0ca7b602f4b554e3b522c324d3fb6bea5d7
William Gentil <william.gentil@protonmail.com>
2025-05-19 11:34:42 +0200

    feat: ✨ atomize components #54 (#60)
    * refactor: 🎨 atomize header component #54
    
    * refactor: 🎨 add collections for blog cards #54
    
    * refactor: 🎨 blog collection #54
    
    * chore: 🔧 update biome run commmand #54
    
    * chore: ⬆️  update PATCH @types/node #54
    
    * chore: 🔧 ignore blog directory for knip #54
    
    * refactor: 🎨 atomize legacy components #54
    
    * refactor: 🎨 add astro-icons instead of local svg files #54
    
    * chore: 🔧 ignore iconify dependencies for knip #54
    
    ---------
    
    Co-authored-by: Yudao <mickael.gentil@gmail.com>
    Co-authored-by: gentil <skabz@DESKTOP-8EG3JAF>

---
+++ src/components/molecules/index.ts
@@ -0,0 +1,2 @@
+export { default as MainMenu } from "./MainMenu.astro";
+export { default as BlogCard } from "./BlogCard.astro";
---
+++ src/components/organisms/index.ts
@@ -0,0 +1,6 @@
+export { default as Header } from "./Header.astro";
+export { default as Footer } from "./Footer.astro";
+export { default as BlogList } from "./BlogList.astro";
+export { default as ContactForm } from "./ContactForm.astro";
+export { default as MegaMenu } from "./MegaMenu.astro";
+export * from "./home";
