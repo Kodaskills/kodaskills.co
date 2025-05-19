dfd5141cbaf1c09cb8136cf5b653c3ac27e5a3ae
Yudao <mickael.gentil@gmail.com>
2025-05-19 10:44:07 +0200

    refactor: 🎨 atomize legacy components #54

---
+++ src/components/organisms/home/sections/index.ts
@@ -0,0 +1,4 @@
+export { default as Blog } from "./Blog.astro";
+export { default as Hero } from "./Hero.astro";
+export { default as Contact } from "./Contact.astro";
+export { default as Service } from "./Service.astro";
