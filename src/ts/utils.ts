6721436a34645959ce8e219f9099e6158f61758b
williamgentil <william.gentil@protonmail.com>
2025-06-04 18:19:04 +0200

    chore: 🔒️ csp directives implementation #81

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -1,3 +1,16 @@
+import fs from "node:fs";
+import path from "node:path";
+import { load } from "js-yaml";
+
+export function getCSPString(): string {
+  const file = fs.readFileSync(path.resolve(".csp.yml"), "utf8");
+  const cspObject = load(file) as Record<string, string>;
+
+  return Object.entries(cspObject)
+    .map(([key, value]) => `${key} ${value}`)
+    .join("; ");
+}
+
 export function formatDate(date: Date) {
   return new Date(date).toLocaleDateString("en-US", {
     timeZone: "UTC",
